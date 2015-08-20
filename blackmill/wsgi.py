ab1d5713f33b18fe1a32f4afb92c0188792fcb7e
weilong <apple@AppledeiMac.local>
2015-08-21 00:12:10 +0800

    blackmill

--- blackmill/wsgi.py
+++ blackmill/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+## mysite/wsgi.py
+
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blackmill.settings")
-
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
