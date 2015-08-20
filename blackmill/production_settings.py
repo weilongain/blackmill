e65afc8abbcdf313479e6d29787570bcbed37eed
apple <weilongchin@gmail.com>
2015-08-21 01:08:37 +0800

    heroku

--- blackmill/production_settings.py
+++ blackmill/production_settings.py
@@ -1,12 +1,12 @@
-from .settings import *
+from settings import *
 
 import dj_database_url
 DATABASES = {
     'default': dj_database_url.config()
 }
 
-# Static asset configuration
-STATIC_ROOT = 'staticfiles'
+# # Static asset configuration
+# STATIC_ROOT = 'staticfiles'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- blackmill/wsgi.py
+++ blackmill/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 ## mysite/wsgi.py
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blackmill.settings")
 
 from django.core.wsgi import get_wsgi_application
 
